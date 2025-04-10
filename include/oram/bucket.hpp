f62e23e73b141edacdcdb798f5ed4405468081a5
unalive <nonadhocproblems@gmail.com>
2025-04-10 22:04:05 +0530

    modification

---
+++ include/oram/bucket.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include "block.hpp"
+
+constexpr int bucket_size = 4;  //Z
+
+class bucket
+{
+private:
+    block b[bucket_size];
+public: 
+};
---
+++ src/server/server_main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    //handle socket stuff
+
+    if("operation is write")
+    {
+
+    }
+    else if("operation is read")
+    {
+
+    }
+
+
+}
